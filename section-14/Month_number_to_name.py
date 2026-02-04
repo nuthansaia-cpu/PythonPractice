3b4c23c9b2612e4830587020a76f43aaf90310e7
nuthansaia-cpu <nuthansai.a@gmail.com>
2026-02-04 14:04:39 +0530

    Clean up temporary files

---
+++ section-14/Month_number_to_name.py
@@ -0,0 +1,28 @@
+month = int(input("Enter month number: "))
+
+if month == 0:
+        print("January")
+elif month == 1:
+    print("February")
+elif month == 2:
+    print("March")
+elif month == 3:
+    print("April")
+elif month == 4:
+    print("May")
+elif month == 5:
+    print("June")
+elif month == 6:
+    print("July")
+elif month == 7:
+    print("August")
+elif month == 8:
+    print("September")
+elif month == 9:
+    print("October")
+elif month == 10:
+    print("November")
+elif month == 11:
+    print("December")
+else:
+    print("Invalid month number")
