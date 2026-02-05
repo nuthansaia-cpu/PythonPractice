c7cd96913c31d3093a638565f4c2efde6bb46d7b
nuthansaia-cpu <nuthansai.a@gmail.com>
2026-02-05 19:03:47 +0530

    added 6 files

---
+++ Section-13/is_and_is_not.py
@@ -0,0 +1,6 @@
+# is
+a="Hello"
+b="Hello"
+print(a is b)
+print(id(a))
+id(b)
---
+++ Section-13/non_boolean_condition.py
@@ -0,0 +1,8 @@
+# Non Boolean Condition with and 
+print(5 and 10)
+print(5 and 0)
+print(0 and 10)
+
+# Non Boolean Condition with or
+print(5 or 10)
+print(0 or 10)
