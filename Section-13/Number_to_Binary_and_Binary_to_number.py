24e1ab1a25c8958e1981f6b0b3e15260739be2c0
nuthansaia-cpu <nuthansai.a@gmail.com>
2026-02-05 19:12:55 +0530

    changed 2 files

---
+++ Section-13/Number_to_Binary_and_Binary_to_number.py
@@ -0,0 +1,16 @@
+a=10
+Binary=format(a,'b')
+print(Binary)
+
+b=14
+Binary=format(b,'b')
+print(Binary)
+
+Binary=format(25,'b')
+print(Binary)
+
+c=25
+Binary=bin(c)
+print(Binary)
+
+c.bit_length()
--- Section-13/is_and_is_not.py
+++ Section-13/is_and_is_not.py
@@ -4,3 +4,10 @@ b="Hello"
 print(a is b)
 print(id(a))
 id(b)
+print("---------------------------------------------------")
+# is not
+c="Hello World"
+d= "hello"
+print(a is not c)
+print(id(c))
+print(id(d))
