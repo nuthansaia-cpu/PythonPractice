6e4d7613673f72d30b3d08299f8676e770cdbc6b
nuthansaia-cpu <nuthansai.a@gmail.com>
2026-02-12 23:05:26 +0530

    added 2 files

---
+++ section-18/Card_Payment_Recepit.py
@@ -0,0 +1,5 @@
+cardno=input('Enter Card No:')
+lastdigits=cardno[15 : : ]
+four='x'*4+' '
+disno=four*3+lastdigits
+print(disno)
---
+++ section-18/Restaurant_Menu.py
@@ -0,0 +1,19 @@
+item1='Hot Dog'
+price1='30'
+dash=20-len(item1)-len(price1)
+print(item1,'-'*dash,'$',price1)
+
+item1='Dount'
+price1='40'
+dash=20-len(item1)-len(price1)
+print(item1,'-'*dash,'$',price1)
+
+item1='Burger'
+price1='35'
+dash=20-len(item1)-len(price1)
+print(item1,'-'*dash,'$',price1)
+
+item1='Pizza'
+price1='65'
+dash=20-len(item1)-len(price1)
+print(item1,'-'*dash,'$',price1)
