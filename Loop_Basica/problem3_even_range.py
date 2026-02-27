55b5e3745eb865c4ef1623d478876f57c2641dd4
nuthansaia-cpu <nuthansai.a@gmail.com>
2026-02-27 22:19:54 +0530

    added all files

--- Loop_Basica/problem3_even_range.py
+++ Loop_Basica/problem3_even_range.py
@@ -1,7 +1,12 @@
 n=int(input('Enter a number:'))
 x=int(input('Enter a number greater then the first entered number:'))
-
-# using for loop
+m=int(input('multiple:'))
+rem=n%m
+add=m-rem
+z=add+n
+for i in range(z,x+1,m):
+	print(i)
+'''# using for loop
 for i in range(n,x+1,1):
 	if i%2==0:
 		print(i,end=' ')
@@ -12,3 +17,11 @@ while n<=x:
 	if n%2==0:
 		print(n,end=' ')
 	n+=1
+if n%m==0:
+	z=n
+elif n%m==1:
+    z=n+2
+else:
+    z=n+1
+for i in range(z,x+1,m):
+	print(i)'''
---
+++ String_Challenge-Intermediate/Word_Mirror_Sentence.py
@@ -0,0 +1,6 @@
+s1=input ('enter a sentence:')
+s2=s1.split()
+print(s2)
+rev=s2[::-1]
+print(rev)
+print(s2.casefold()+rev.casefold())
