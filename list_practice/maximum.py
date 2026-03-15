06ee55a1b0116fe0055b8c40b3d72a0624f2552e
nuthansaia-cpu <nuthansai.a@gmail.com>
2026-03-15 20:59:51 +0530

    chagend 2 Files

--- list_practice/maximum.py
+++ list_practice/maximum.py
@@ -1,4 +1,4 @@
-l1=[-1,35,36,37,40,38,39,40,-2]
+'''l1=[-1,35,36,37,40,38,39,40,-2]
 res=[]
 max=float('-inf')
 y=0
@@ -8,8 +8,23 @@ for i in range(len(l1)):
 	if y>max:
 		max=y
 		z=i
-print(max,'number found at index',z)
+print(max,'number found at index',z)'''
+	
+l1 = [-1, 35, 36, 37, 40, 38, 39, 40, -2]
+res = []
+y=0
+z=0
+for x in range(len(l1)):
+    max = float('-inf')
+    for i in range(len(l1)):
+        y=l1[i]
+        if y> max:
+            max = y
+            z = i
 
+    del l1[z]
+    res.append(max)
+    print(res)
+    print(max, 'number found at index', z)
 
-
-
+ 
--- list_practice/minimum.py
+++ list_practice/minimum.py
@@ -1,4 +1,4 @@
-l1=[-5,-10,-20,-30]
+'''l1=[-5,-10,-20,-30]
 min=float('inf')
 print(min)
 y=0
@@ -8,4 +8,22 @@ for i in range(len(l1)):
 	if y<min:
 		min=y
 		z=i
-print(min,'number found at index',z)
+print(min,'number found at index',z)'''
+
+l1 = [-1, 35, 36, 37, 40, 38, 39, 40, -2]
+res = []
+y=0
+z=0
+for x in range(len(l1)):
+    min = float('inf')
+    for i in range(len(l1)):
+        y=l1[i]
+        if y<min:
+            min = y
+            z = i
+
+    del l1[z]
+    res.append(min)
+
+    print(res)
+    print(min, 'number found at index', z)
