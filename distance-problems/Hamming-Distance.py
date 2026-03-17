d315703dbd0c757998be5d33e0db4e2c6d6acb82
nuthansaia-cpu <nuthansai.a@gmail.com>
2026-03-17 21:56:34 +0530

    changed 1 file

--- distance-problems/Hamming-Distance.py
+++ distance-problems/Hamming-Distance.py
@@ -20,9 +20,9 @@ else:
 
 for i in range(x):
 	if l1[i]==l2[i]:
-		print('',end='')
+		print('Position',i,':', l1[i],'and',l2[i],'→ Same')
 	else:
 		count+=1
-
+		print('Position',i,':', l1[i],'and',l2[i],'→ Different')
 
 print('Hamming distance of the given two string is:',count)
