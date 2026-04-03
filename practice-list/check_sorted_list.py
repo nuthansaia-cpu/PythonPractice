ec8d77fb59705f1b6ecdaef1da482ee6fc36d947
nuthansaia-cpu <nuthansai.a@gmail.com>
2026-04-04 00:04:40 +0530

    changed 1 file

--- practice-list/check_sorted_list.py
+++ practice-list/check_sorted_list.py
@@ -1,4 +1,4 @@
-l1=[2,4,4,7,9,10]
+'''l1=[1,2,3]
 l2=l1
 l3=len(l1)
 count=0
@@ -17,4 +17,17 @@ while n<=0:
 if count1==l3:
 		print('It is a sorted list')
 else:
-		print('It is a unsorted list')
+		print('It is a unsorted list')'''
+
+l1=[2,4,4,7,9,10]
+count=1
+for i in range(1,len(l1)):
+	if l1[i]>=l1[i-1]:
+		count+=1
+	else:
+		if l1[i]>l1[i-1]:
+			break
+if count==len(l1):
+	print('It is a sorted list')
+else:
+	print('It is a unsorted list')
